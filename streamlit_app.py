af4bd572937cd8bb76f8120113043dd135fb4c0c
KoryClik <corina.bioinformatic@gmail.com>
2023-10-10 00:39:00 +0100

    Update streamlit_app.py
    add choice fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
 st.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #st.text(fruityvice_response.json())
 
 # write your own comment -what does the next line do? 
