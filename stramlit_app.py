1637413a8c7ded9fed0ad40e58951ed1137f921b
KoryClik <corina.bioinformatic@gmail.com>
2023-10-09 20:29:59 +0100

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-streamlit.title('My Parents New Healthy Diner')
-streamlit.subtitle('Breakfast Menu')
+st.title('My Parents New Healthy Diner')
+st.subtitle('Breakfast Menu')
 
 st.text('Omega 3 % Blueberry Oatmeal')
 st.text('Kale, Spinach & Rocket smoothie')
