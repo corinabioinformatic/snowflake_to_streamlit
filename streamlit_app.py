c9594359339300803242fc12fd3d0a058843ed61
KoryClik <corina.bioinformatic@gmail.com>
2023-10-09 20:53:10 +0100

    Update streamlit_app.py
    added fruit table and pd

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,6 @@ st.text('🍞 beans in toast')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
