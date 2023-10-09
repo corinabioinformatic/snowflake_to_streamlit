7958ad583598b5fd1bec3132c0662f183afc3d93
KoryClik <corina.bioinformatic@gmail.com>
2023-10-09 20:31:14 +0100

    Update stramlit_app.py
    reformat text

--- stramlit_app.py
+++ stramlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('My Parents New Healthy Diner')
-st.subtitle('Breakfast Menu')
+st.title('Breakfast Menu')
 
 st.text('Omega 3 % Blueberry Oatmeal')
 st.text('Kale, Spinach & Rocket smoothie')
-
 st.text('Hard-Boiled Free-Range Egg')
