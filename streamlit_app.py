f3f1c91d91f69da62f77ba7b8c3a01bec01c2ea6
KoryClik <corina.bioinformatic@gmail.com>
2023-10-10 16:15:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ my_data_rows = my_cur.fetchall()
 
 st.header("The fruit load list contains:")
 #----
-my_unlisted_rows = [x for x in my_data_rows]
+my_unlisted_rows = pd.explode(my_data_rows)
 st.text(type(my_data_rows))
 st.text(my_unlisted_rows)
 
