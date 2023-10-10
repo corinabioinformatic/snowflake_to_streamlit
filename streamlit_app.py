f308687fc537cc01ceee365e02bbd3b531a9627a
KoryClik <corina.bioinformatic@gmail.com>
2023-10-10 18:25:23 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ st.dataframe(fruits_to_show)
 ###### FRUIT SELECTION
 st.header("Fruityvice Fruit Advice!")
 try:   
-   fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+   fruit_choice = st.text_input('What fruit would you like information about?')
    if not fruit_choice:
       st.error("Please select a fruit to get information")
    else: 
