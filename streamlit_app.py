b9c9b1c7c4587263de67a9deb6dd87cec0e91813
KoryClik <corina.bioinformatic@gmail.com>
2023-10-10 15:34:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,15 @@ import requests
 import snowflake.connector
 
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
+
+
 st.title('My Parents New Healthy Diner')
 st.header('Breakfast Menu')
    
